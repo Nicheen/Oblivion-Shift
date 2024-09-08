1766b8ffcc411ba6a21619923d2c1b05d598cf63
Nicheen <46378903+Nicheen@users.noreply.github.com>
2024-09-08 10:00:08 +0200

    Added projectiles
    - entity_create() function was created to add each new entity to a list called entities.
    
    - entity_destroy() makes it so you can remove a entity from memory.
    
    - setup_projectiles() is a setup function for projectiles which has different parameters for the entity then the player.
    
    - The player now has a check for the mouse1 button, which will spawn a projectile at the players position.
    
    - A new way of drawing the entities with a draw loop. The loop goes through every entity in the entities list and checks if the entity is valid and if the entity is outside of bounds. It also checks if a circle or rectangle is supposed to be drawn.

--- main_game_loop.c
+++ main_game_loop.c
@@ -1,15 +1,46 @@
+#define MAX_ENTITY_COUNT 1024
+
 typedef struct Entity {
 	Vector2 size;
 	Vector2 position;
 	Vector2 velocity;
 	Vector4 color;
-	bool is_alive;
+	bool is_circle;
+	bool is_valid;
 } Entity;
+// A list to hold all the entities
+Entity entities[MAX_ENTITY_COUNT];
+
+Entity* entity_create() {
+	Entity* entity_found = 0;
+	for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+		Entity* existing_entity = &entities[i];
+		if (!existing_entity->is_valid) {
+			entity_found = existing_entity;
+			break;
+		}
+	}
+	assert(entity_found, "No more free entities!");
+	entity_found->is_valid = true;
+	return entity_found;
+}
+
+void entity_destroy(Entity* entity) {
+	memset(entity, 0, sizeof(Entity));
+}
 
 void setup_player(Entity* entity) {
 	entity->size = v2(100, 20);
 	entity->position = v2(0, -300);
 	entity->color = COLOR_WHITE;
+}
+
+void setup_projectile(Entity* entity, Entity* player) {
+	entity->size = v2(10, 10);
+	entity->position = v2(player->position.x + player->size.x / 2, player->position.y + player->size.y);
+	entity->color = v4(0, 1, 0, 1); // Green color
+	entity->is_circle = true;
+	entity->velocity = v2(0, get_random_int_in_range(100, 200));
 }
 
 int entry(int argc, char **argv) {
@@ -25,8 +56,11 @@ int entry(int argc, char **argv) {
 	float64 last_time = os_get_elapsed_seconds();
 
 	// Here we create the player entity object
-	struct Entity player;
-	setup_player(&player);
+	Entity* player = entity_create();
+	setup_player(player);
+
+	
+
 	// --------------------------------
 
 	while (!window.should_close) {
@@ -38,30 +72,58 @@ int entry(int argc, char **argv) {
 		last_time = now;
 
 		// main code loop here --------------
-		// Here we draw the player as a rectangle
+		if (player->is_valid)
 		{
+			if (is_key_just_pressed(MOUSE_BUTTON_LEFT)) 
+			{
+				consume_key_just_pressed(MOUSE_BUTTON_LEFT);
+				Entity* projectile = entity_create();
+				setup_projectile(projectile, player);
+			}
+
 			Vector2 input_axis = v2(0, 0); // Create an empty 2-dim vector
-			if (is_key_down('A') && player.position.x > -window.point_width / 2 - player.size.x / 2) {
+			
+			if (is_key_down('A') && player->position.x > -window.point_width / 2 - player->size.x / 2)
+			{
 				input_axis.x -= 1.0;
 			}
-			if (is_key_down('D') && player.position.x <  window.point_width / 2 - player.size.x / 2) {
+
+			if (is_key_down('D') && player->position.x <  window.point_width / 2 - player->size.x / 2)
+			{
 				input_axis.x += 1.0;
 			}
 			
 			input_axis = v2_normalize(input_axis);
-			player.position = v2_add(player.position, v2_mulf(input_axis, 500.0 * delta_t));
+			player->position = v2_add(player->position, v2_mulf(input_axis, 500.0 * delta_t));
+		}
+
+		// Entity Loop for drawing and everything else
+		int entity_counter = 0;
+		for (int i = 0; i < MAX_ENTITY_COUNT; i++) { // Here we loop through every entity
+			Entity* entity = &entities[i];
+			if (entity->is_valid) { // Check if the entity is valid or not
+				entity_counter += 1;
+				entity->position = v2_add(entity->position, v2_mulf(entity->velocity, delta_t));
+
+				if (entity->position.y > window.height) {
+					entity_destroy(entity);
+				}
+
+				if (entity->is_circle) {
+					draw_circle(entity->position, entity->size, entity->color);
+				} else {
+					draw_rect(entity->position, entity->size, entity->color);
+				}
+			}
 		}
 		
-		draw_rect(player.position, player.size, player.color);
-		draw_circle(player.position, v2(5, 5), COLOR_RED); // Player position origin
-
-		// main code loop here --------------
+		// main code loop here --------------a
 		os_update(); 
 		gfx_update();
 		seconds_counter += delta_t;
 		frame_count += 1;
 		if (seconds_counter > 1.0) {
-			log("fps: %i", frame_count);
+			log("fps: %i, n-entities: %i", frame_count, entity_counter);
 			seconds_counter = 0.0;
 			frame_count = 0;
 		}
